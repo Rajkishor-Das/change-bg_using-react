53d8a074497a24e5e952d10dd06ef8f759d551ba
Rajkishor Das <dasrajkishor15@gmail.com>
2023-10-31 21:26:27 +0530

    Simple background change color ( Using React JS )

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [bg, setBg] = useState('indigo');
+  const name = ['red', 'yellow', 'green', 'blue', 'black', 'cyan', 'coral', 'orangered', 'pink'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full h-screen duration-200" style={{ backgroundColor: bg }}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 mx-5">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            {
+              name.map((curElem, i) => {
+                return (
+                  <Button name={curElem} bg={() => setBg(curElem)} />
+                );
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+function Button({ name, bg }) {
+  return (
+    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg capitalize" style={{ backgroundColor: name }} onClick={bg}>{name}</button>
   );
 }
 
